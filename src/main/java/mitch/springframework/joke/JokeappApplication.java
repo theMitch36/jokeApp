ffb0e92bc75b678afd2b58c0ce34e625ad14c20f
theMitch36 <devmakos36@gmail.com>
2018-02-22 15:38:40 -0500

    first commit

---
+++ src/main/java/mitch/springframework/joke/JokeappApplication.java
@@ -0,0 +1,12 @@
+package mitch.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeappApplication.class, args);
+	}
+}
